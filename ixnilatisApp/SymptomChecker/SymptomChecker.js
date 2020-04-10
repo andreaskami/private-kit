97ca45a7bcd4466eb94fca424b9deb42796412d4
Filippos Karailanidis <phil.230@gmail.com>
2020-04-10 21:03:54 +0300

    Display results

--- ixnilatisApp/SymptomChecker/SymptomChecker.js
+++ ixnilatisApp/SymptomChecker/SymptomChecker.js
@@ -10,8 +10,21 @@ import { checkSymptoms } from '../httpClient';
 import { showMessage } from 'react-native-flash-message';
 import { getUniqueId } from 'react-native-device-info';
 
+function removeLineBreaks(string) {
+  return string.replace(/(\r\n|\n|\r|\s+)/gm, ' ');
+}
+function cleanResponse(response) {
+  return {
+    textArray: {
+      title: removeLineBreaks(response.textArray.title),
+      result: removeLineBreaks(response.textArray.result),
+      prompt: removeLineBreaks(response.textArray.prompt),
+      instructions: response.textArray.instructions.map(removeLineBreaks),
+    },
+  };
+}
 export default function SymptomChecker(props) {
-  const [results, setResults] = React.useState(sampleResult);
+  const [results, setResults] = React.useState(null);
   const [isLoading, setLoading] = React.useState(false);
 
   async function submitForm(data) {
@@ -37,14 +50,12 @@ export default function SymptomChecker(props) {
       covid_19_contact: data.hasBeenExposed ? 1 : 0,
       chest_pain: data.symptoms.has('chest_pain') ? 1 : 0,
       nothing: data.symptoms.has('none_of_the_above') ? 1 : 0,
-      // unique_identification: 'unknown',
-      // lat: '0.00',
-      // long: '0.00',
     };
     setLoading(true);
     const languages = await getLanguages();
-    const userLang = languages[0].split('-')[0]; // ['en-US' will become 'en']
+    const userLang = languages[0].split('-')[0];
     checkSymptoms(request, userLang)
+      .then(cleanResponse)
       .then(setResults)
       .catch(err =>
         showMessage({
@@ -65,7 +76,7 @@ export default function SymptomChecker(props) {
       {results === null ? (
         <Form onSubmit={submitForm} />
       ) : (
-        <Results results={results} />
+        <Results results={results.textArray} />
       )}
     </View>
   );
@@ -80,23 +91,3 @@ const styles = StyleSheet.create({
     backgroundColor: colors.WHITE,
   },
 });
-
-const sampleResult = {
-  html:
-    '<h1><b>Your travel history and symptoms are indicative of a respiratory virus, which may be COVID-19.</b></h1> <p style="text-align: center; padding-left: 10px"> <br/> <p class="text-center"><strong>According to official instructions you should act as follows:</strong></p> <ul style="text-align: left;float: left;padding-left: 10%;"> <li> Contact your general practitioner who will arrange your further examination and treatment.* </li> <li>Remain in quarantine for two weeks.  </li> <li>The COVID19 Emergency Response Line (1420) should be called only in case of emergency.</li> <li>Inform those you have been in close contact with to monitor their health, stay home if possible, and to fill out this self-assessment questionnaire for further instructions if necessary </li> </ul> </p>',
-  suggest: 'CASE4',
-  text:
-    'Your travel history and symptoms are indicative of a respiratory virus, which may be COVID-19. |   |  According to official instructions you should act as follows: |  Contact your general practitioner who will arrange your further examination and treatment.* |  Remain in quarantine for two weeks. |  The COVID19 Emergency Response Line (1420) should be called only in case of emergency. |  Inform those you have been in close contact with to monitor their health, stay home if possible, and to fill out this self-assessment questionnaire for further instructions if necessary',
-  textArray: {
-    instructions: [
-      'Contact your general practitioner who will arrange your further examination and treatment.*',
-      'Remain in quarantine for two weeks.',
-      'The COVID19 Emergency Response Line (1420) should be called only in case of emergency.',
-      'Inform those you have been in close contact with to monitor their health, stay home if possible, and to fill out this self-assessment questionnaire for further instructions if necessary',
-    ],
-    prompt: 'According to official instructions you should act as follows:',
-    result: '',
-    title:
-      'Your travel history and symptoms are indicative of a respiratory virus, which may be COVID-19.',
-  },
-};
