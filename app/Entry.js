6df2b49e4ca5fdd5280ac0a5d098f27be7d9f898
Petros Kyriakou <9pitops@gmail.com>
2020-04-27 20:17:02 +0400

    switch to pitops eslint

--- app/Entry.js
+++ app/Entry.js
@@ -1,112 +1,108 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaView } from 'react-native';
-import LocationTracking from './views/LocationTracking';
-import Welcome from './views/Welcome';
-import NewsScreen from './views/News';
-import ExportScreen from './views/Export';
-import ImportScreen from './views/Import';
-import OverlapScreen from './views/Overlap';
-import LicencesScreen from './views/Licenses';
-import StatisticsScreen from './views/Statistics';
-import Slider from './views/welcomeScreens/Slider';
-import { GetStoreData } from './helpers/General';
-import { getIxnilatisScreens } from '../ixnilatisApp/IxnilatisScreens';
-import FlashMessage from 'react-native-flash-message';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { SafeAreaView } from 'react-native'
+import LocationTracking from './views/LocationTracking'
+import Welcome from './views/Welcome'
+import NewsScreen from './views/News'
+import ExportScreen from './views/Export'
+import ImportScreen from './views/Import'
+import OverlapScreen from './views/Overlap'
+import LicencesScreen from './views/Licenses'
+import StatisticsScreen from './views/Statistics'
+import Slider from './views/welcomeScreens/Slider'
+import { GetStoreData } from './helpers/General'
+import { getIxnilatisScreens } from '../ixnilatisApp/IxnilatisScreens'
+import FlashMessage from 'react-native-flash-message'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 class Entry extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       initialRouteName: '',
-    };
+    }
   }
 
   componentDidMount() {
     GetStoreData('PARTICIPATE')
-      .then(isParticipating => {
-        console.log(isParticipating);
+      .then((isParticipating) => {
+        console.log(isParticipating)
         this.setState({
           initialRouteName: isParticipating,
-        });
+        })
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error))
   }
 
   render() {
     return (
       <NavigationContainer>
         <SafeAreaView style={{ flex: 1 }}>
-          <Stack.Navigator initialRouteName='InitialScreen'>
+          <Stack.Navigator initialRouteName="InitialScreen">
             {this.state.initialRouteName === 'true' ? (
               <Stack.Screen
-                name='InitialScreen'
+                name="InitialScreen"
                 component={LocationTracking}
                 options={{ headerShown: false }}
               />
             ) : (
               <Stack.Screen
-                name='InitialScreen'
+                name="InitialScreen"
                 component={Slider}
                 options={{ headerShown: false }}
               />
             )}
+            <Stack.Screen name="Slider" component={Slider} options={{ headerShown: false }} />
             <Stack.Screen
-              name='Slider'
-              component={Slider}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name='WelcomeScreen'
+              name="WelcomeScreen"
               component={Welcome}
               options={{ headerShown: false }}
             />
             <Stack.Screen
-              name='LocationTrackingScreen'
+              name="LocationTrackingScreen"
               component={LocationTracking}
               options={{ headerShown: false }}
             />
             <Stack.Screen
-              name='NewsScreen'
+              name="NewsScreen"
               component={NewsScreen}
               options={{ headerShown: false }}
             />
             <Stack.Screen
-              name='ExportScreen'
+              name="ExportScreen"
               component={ExportScreen}
               options={{ headerShown: false }}
             />
             <Stack.Screen
-              name='ImportScreen'
+              name="ImportScreen"
               component={ImportScreen}
               options={{ headerShown: false }}
             />
             <Stack.Screen
-              name='LicensesScreen'
+              name="LicensesScreen"
               component={LicencesScreen}
               options={{ headerShown: false }}
             />
             <Stack.Screen
-              name='StatisticsScreen'
+              name="StatisticsScreen"
               component={StatisticsScreen}
               options={{ headerShown: false }}
             />
             <Stack.Screen
-              name='OverlapScreen'
+              name="OverlapScreen"
               component={OverlapScreen}
               options={{ headerShown: false }}
             />
             {getIxnilatisScreens(Stack)}
           </Stack.Navigator>
-          <FlashMessage ref='top' />
+          <FlashMessage ref="top" />
         </SafeAreaView>
       </NavigationContainer>
-    );
+    )
   }
 }
 
-export default Entry;
+export default Entry
