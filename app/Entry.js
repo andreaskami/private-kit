8675106310c2fbeb016eb7f27cf5acd932765fe8
Stefanos Chrs <root@stefanoschrs.com>
2020-05-02 19:41:39 +0200

    - refactor Entry - fix onboarding flicker

--- app/Entry.js
+++ app/Entry.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
+import FlashMessage from 'react-native-flash-message'
+import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
@@ -11,19 +13,43 @@ import OverlapScreen from './views/Overlap'
 import LicencesScreen from './views/Licenses'
 import { StatisticsScreen } from './views/Statistics'
 import { OnboardingSlider } from './views/Onboarding/Slider'
-import { GetStoreData } from './helpers/General'
-import FlashMessage from 'react-native-flash-message'
-import { SafeAreaView } from 'react-native'
 import FormWork from './views/FormWork'
 import FormGeneralNew from './views/FormGeneralNew'
 import { Acknowledgements } from './views/Acknowledgements'
 import { Privacy } from './views/Privacy'
 import SymptomChecker from './views/SymptomChecker/SymptomChecker'
+import { GetStoreData } from './helpers/General'
 
 const Stack = createStackNavigator()
 
+const Pages = [
+  { name: 'Slider', component: OnboardingSlider, hasHeader: false },
+  { name: 'HomeScreen', component: Home, hasHeader: false },
+  { name: 'OnboardingScreen', component: OnboardingSlider, hasHeader: false },
+  { name: 'NewsScreen', component: NewsScreen, hasHeader: false },
+  { name: 'ExportScreen', component: ExportScreen, hasHeader: false },
+  { name: 'ImportScreen', component: ImportScreen, hasHeader: false },
+  { name: 'LicensesScreen', component: LicencesScreen, hasHeader: false },
+  { name: 'StatisticsScreen', component: StatisticsScreen, hasHeader: false },
+  { name: 'OverlapScreen', component: OverlapScreen, hasHeader: false },
+  { name: 'FormWorkScreen', component: FormWork, hasHeader: false },
+  { name: 'FormGeneralNewScreen', component: FormGeneralNew, hasHeader: false },
+  { name: 'PrivacyScreen', component: Privacy, hasHeader: false },
+  { name: 'AckScreen', component: Acknowledgements, hasHeader: false },
+  { name: 'SymptomCheckerScreen', component: SymptomChecker, hasHeader: false }
+]
+
 export const Entry = () => {
   const [isOnboard, setIsOnboard] = useState(false)
+
+  const pages = Pages.map(page => (
+    <Stack.Screen
+      key={page.name}
+      name={page.name}
+      component={page.component}
+      options={{ headerShown: page.hasHeader }}
+    />
+  ))
 
   useEffect(() => {
     GetStoreData('ONBOARDING_COMPLETE')
@@ -36,79 +62,10 @@ export const Entry = () => {
   return (
     <NavigationContainer>
       <SafeAreaView style={{ flex: 1 }}>
-        <Stack.Navigator initialRouteName='InitialScreen'>
-          {isOnboard ? (
-            <Stack.Screen name='InitialScreen' component={Home} options={{ headerShown: false }} />
-          ) : (
-            <Stack.Screen
-              name='InitialScreen'
-              component={OnboardingSlider}
-              options={{ headerShown: false }}
-            />
-          )}
-          <Stack.Screen
-            name='Slider'
-            component={OnboardingSlider}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name='HomeScreen' component={Home} options={{ headerShown: false }} />
-          <Stack.Screen name='NewsScreen' component={NewsScreen} options={{ headerShown: false }} />
-          <Stack.Screen
-            name='ExportScreen'
-            component={ExportScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name='ImportScreen'
-            component={ImportScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name='LicensesScreen'
-            component={LicencesScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name='StatisticsScreen'
-            component={StatisticsScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name='OverlapScreen'
-            component={OverlapScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            key='FormWorkScreen'
-            name='FormWorkScreen'
-            component={FormWork}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            key='FormGeneralNewScreen'
-            name='FormGeneralNewScreen'
-            component={FormGeneralNew}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            key='PrivacyScreen'
-            name='PrivacyScreen'
-            component={Privacy}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            key='AckScreen'
-            name='AckScreen'
-            component={Acknowledgements}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            key='SymptomCheckerScreen'
-            name='SymptomCheckerScreen'
-            component={SymptomChecker}
-            options={{ headerShown: false }}
-          />
+        <Stack.Navigator initialRouteName={isOnboard ? 'OnboardingScreen' : 'HomeScreen'}>
+          {pages}
         </Stack.Navigator>
+
         <FlashMessage
           ref={ref => {
             this.top = ref
